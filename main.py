4d23b6f6266a59c4c1ca029eb0ca53abebb8df31
Hasan Fuad <fuadifty44@gmail.com>
2025-07-29 01:43:57 +0200

    Added the 3nd section

--- main.py
+++ main.py
@@ -20,11 +20,32 @@ def data_collection():
     
     # 2nd section: User's own short story 
     print("\n--- Your Story in short---")
-    print("Write a short intro, then a short description about yourself.(Type DONE when you want to finish)")
+    print("Write a short intro, then a short description about yourself.(Type FINISH when you want to finish)")
     story_lines = []
     while True:
         line = input()
-        if line.upper() == "DONE":
+        if line.upper() == "FINISH":
             break
         story_lines.append(line)
-    data['Story'] = "\n".join(story_lines)    
+    data['Story'] = "\n".join(story_lines)
+    
+    # 3rd section: Collection Educational information
+    print("\n-- Educational Information --")
+    print("\n enter your educational information according to the prompts, type FINISH to end")
+    
+    edu_entries = []
+    while True:
+        degree = get_user_info_input("Enter your degree (eg. BSc in CE): ")
+        if degree.upper() == "FINISH":
+            break
+    university = get_user_info_input("Enter your university name: ")
+    year = get_user_info_input("Enter the year of graduation (OPTIONAL): ")
+    gpa = get_user_info_input("Enter your GPA: ")
+    edu_entries.appened({
+        'Degree': degree,
+        'University': university,
+        'Year': year,
+        'GPA': gpa
+    })
+    data['Education'] = edu_entries        
+        
